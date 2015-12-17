b7392a27f5089da4d33cb23c50fa5a8fd679b6e4
Zack Breakstone <z.breakstone@gmail.com>
2015-12-16 22:31:32 -0500

    Get OAuth up and running

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -12,8 +12,8 @@ var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = '03ffe0cac0a0401aa6673c3cf6d02ced'; // Your client id
-var client_secret = 'a57c43efb9644574a96d6623fb8bfbc2'; // Your client secret
+var client_id = 'dda6b014f2314fca9283966e29aba562'; // Your client id
+var client_secret = '{secret id}'; // Your client secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
